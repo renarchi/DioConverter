2dc0a0b46444edd1ea4a1b830fe027a0100ae682
renarchi <159624970+renarchi@users.noreply.github.com>
2024-06-23 18:00:07 +0300

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import tkinter as tk
+import os
+import sys
 from ui import App
 import logging_config  # Log konfigürasyonunu dahil ettik
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 if __name__ == "__main__":
     root = tk.Tk()
     app = App(root)
-    root.iconbitmap("Dio.ico")  # Add your icon path here
     root.mainloop()
